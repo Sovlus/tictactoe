a1e4b4a44d2049836e1c3d0f1a26d5d5575fcc63
Sovlus <kubbaz122@gmail.com>
2023-05-15 20:07:42 +0200

    created 9 values

--- src/main.js
+++ src/main.js
@@ -3,3 +3,18 @@ import React from "react";
 function kwadrat({ value }) {
   return <button className='kwadrat'>{value}</button>;
 }
+
+function nineSquares() {
+  return (
+    <div className='gameBoard'>
+      <kwadrat value={"X"} />
+      <kwadrat value={"0"} />
+      <kwadrat value={"X"} />
+      <kwadrat value={"0"} />
+      <kwadrat value={"X"} />
+      <kwadrat value={"0"} />
+      <kwadrat value={"X"} />
+      <kwadrat value={"0"} />
+    </div>
+  );
+}
