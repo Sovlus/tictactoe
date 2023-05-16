86cbc989d1f992ecfe7d14874f91cdd2402d4cd3
Sovlus <kubbaz122@gmail.com>
2023-05-16 16:26:44 +0200

    fixed rows

--- src/main.js
+++ src/main.js
@@ -22,19 +22,19 @@ function Main() {
   return (
     <div className='gameBoard'>
       <div className='row'>
-        <div className='kwadrat'>X</div>
-        <div className='kwadrat'>0</div>
-        <div className='kwadrat'>X</div>
+        <Kwadrat1 value={squares[0]} onClick={() => handleClick(0)} />
+        <Kwadrat1 value={squares[1]} onClick={() => handleClick(1)} />
+        <Kwadrat1 value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className='row'>
-        <div className='kwadrat'>0</div>
-        <div className='kwadrat'>X</div>
-        <div className='kwadrat'>0</div>
+        <Kwadrat1 value={squares[3]} onClick={() => handleClick(3)} />
+        <Kwadrat1 value={squares[4]} onClick={() => handleClick(4)} />
+        <Kwadrat1 value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className='row'>
-        <div className='kwadrat'>X</div>
-        <div className='kwadrat'>0</div>
-        <div className='kwadrat'>X</div>
+        <Kwadrat1 value={squares[6]} onClick={() => handleClick(6)} />
+        <Kwadrat1 value={squares[7]} onClick={() => handleClick(7)} />
+        <Kwadrat1 value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
