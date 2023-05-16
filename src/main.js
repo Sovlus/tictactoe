c95e51afcb882a39aabef45e494a6ea882a7aaa3
Sovlus <kubbaz122@gmail.com>
2023-05-16 16:02:23 +0200

    changed rows

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import React from "react";
 function Main() {
   return (
     <div className='gameBoard'>
-      <div className='row1'>
+      <div className='row'>
         <div className='kwadrat'>X</div>
         <div className='kwadrat'>0</div>
         <div className='kwadrat'>X</div>
       </div>
-      <div className='row2'>
+      <div className='row'>
         <div className='kwadrat'>0</div>
         <div className='kwadrat'>X</div>
         <div className='kwadrat'>0</div>
       </div>
-      <div className='row3'>
+      <div className='row'>
         <div className='kwadrat'>X</div>
         <div className='kwadrat'>0</div>
         <div className='kwadrat'>X</div>
