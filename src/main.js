7282e158780bf8c481dacf57ced11ca1a041bf0b
Sovlus <kubbaz122@gmail.com>
2023-05-16 16:00:36 +0200

    added rows

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ import React from "react";
 function Main() {
   return (
     <div className='gameBoard'>
-      <div className='kwadrat'>X</div>
-      <div className='kwadrat'>0</div>
-      <div className='kwadrat'>X</div>
-      <div className='kwadrat'>0</div>
-      <div className='kwadrat'>X</div>
-      <div className='kwadrat'>0</div>
-      <div className='kwadrat'>X</div>
-      <div className='kwadrat'>0</div>
+      <div className='row1'>
+        <div className='kwadrat'>X</div>
+        <div className='kwadrat'>0</div>
+        <div className='kwadrat'>X</div>
+      </div>
+      <div className='row2'>
+        <div className='kwadrat'>0</div>
+        <div className='kwadrat'>X</div>
+        <div className='kwadrat'>0</div>
+      </div>
+      <div className='row3'>
+        <div className='kwadrat'>X</div>
+        <div className='kwadrat'>0</div>
+        <div className='kwadrat'>X</div>
+      </div>
     </div>
   );
 }
